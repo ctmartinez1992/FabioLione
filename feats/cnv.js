26e118caa3d16e09639bcdb7547ae4428150f720
ctmartinez1992 <ctmartinez1992@gmail.com>
2019-08-31 23:57:44 +0100

    Help section for conversion module (WIP)

--- feats/cnv.js
+++ feats/cnv.js
@@ -1,16 +1,110 @@
-const moment = require('moment');
 const request = require('bluebird').promisifyAll(require('request'), { multiArgs: true });
 
-const utils = require('./../utils');
+var convert = require('convert-units');
+
+all_currencies = [
+    "CAD", "HKD", "ISK", "PHP", "DKK", "HUF", "CZK", "AUD", "RON", "SEK", "IDR", "INR", "BRL", "RUB", "HRK",
+    "JPY", "THB", "CHF", "SGD", "PLN", "BGN", "TRY", "CNY", "NOK", "NZD", "ZAR", "USD", "MXN", "ILS", "GBP",
+    "KRW", "MYR", "EUR"
+];
 
 module.exports = {
     CnvCurrencyCommand: function(args, receivedCommand) {
-        if (args.length == 3) {
+        if (args.length === 0) {
+            receivedCommand.channel.send(`Fabio will convert values between units. Use "\\help cnv for more information."`);
+        } else if (args.length === 1) {
+            if (args[0] === "list") {
+                receivedCommand.channel.send(`Here are all the possible categories of units to choose from: Currency, Length, Are, Mass, Volume, Volume Flow Rate, Temperature, Time, Frequency, Speed, Pace, Pressure, Digital, Illuminance, Parts-Per, Voltage, Current, Power, Apparent Power, Reactive Power, Energy, Reactive Energy, Angle (deg, rad, grad, arcmin, arcsec)`);
+            } else {
+                receivedCommand.channel.send(args[0].concat(" is an invalid argument."));
+            }
+        } else if (args.length === 2) {
+            if (args[0] === "list" && args[1] === "all") {
+                receivedCommand.channel.send(`\nHere are all the possible units:
+* Currency (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)
+* Length (mm, cm, m, in, ft-us, ft, mi)
+* Area (mm2, cm2, m2, ha, km2, in2, ft2, ac, mi2)
+* Mass (mcg, mg, g, kg, oz, lb, mt, t)
+* Volume (mm3, cm3, ml, l, kl, m3, km3, tsp, Tbs, in3, fl-oz, cup, pnt, qt, gal, ft3, yd3)
+* Temperature (C, F, K, R)
+* Time (ns, mu, ms, s, min, h, d, week, month, year
+* Frequency (Hz, mHz, kHz, MHz, GHz, THz, rpm, deg/s, rad/s)
+* Speed (m/s, km/h, m/h, knot, ft/s)
+* Pace (s/m, min/km, s/ft, min/km)
+* Pressure (Pa, hPa, kPa, MPa, bar, torr, psi, ksi)
+* Digital (b, Kb, Mb, Gb, Tb, B, KB, MB, GB, TB)
+* Illuminance (lx, ft-cd)
+* Parts-Per (ppm, ppb, ppt, ppq)
+* Voltage (V, mV, kV)
+* Current (A, mA, kA)
+* Power (W, mW, kW, MW, GW)
+* Apparent Power (VA, mVA, kVA, MVA, GVA)
+* Reactive Power (VAR, mVAR, kVAR, MVAR, GVAR)
+* Energy (Wh, mWh, kWh, MWh, GWh, J, kJ)
+* Reactive Energy (VARh, mVARh, kVARh, MVARh, GVARh)
+* Angle (deg, rad, grad, arcmin, arcsec)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "length") {
+                receivedCommand.channel.send(`\nHere are all length units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "area") {
+                receivedCommand.channel.send(`\nHere are all area units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "mass") {
+                receivedCommand.channel.send(`\nHere are all mass units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "volume") {
+                receivedCommand.channel.send(`\nHere are all volume units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "temperature") {
+                receivedCommand.channel.send(`\nHere are all temperature units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "time") {
+                receivedCommand.channel.send(`\nHere are all time units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "frequency") {
+                receivedCommand.channel.send(`\nHere are all frequency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "speed") {
+                receivedCommand.channel.send(`\nHere are all speed units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else if (args[0] === "list" && args[1] === "currency") {
+                receivedCommand.channel.send(`\nHere are all currency units: (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)`);
+            } else {
+                receivedCommand.channel.send("Invalid argument.");
+            }
+        }
+        if (args.length === 3) {
             const value = Number(args[0]);
-            const from = args[1].toUpperCase();
-            const to = args[2].toUpperCase();
+            const from = args[1];
+            const to = args[2];
 
-            _cnv_currency(receivedCommand, value, from, to)
+            if (all_currencies.includes(from.toUpperCase()) && all_currencies.includes(to.toUpperCase())) {
+                _cnv_currency(receivedCommand, value, from.toUpperCase(), to.toUpperCase());
+            } else {
+                try {
+                    const convertedValue = convert(value).from(from).to(to);
+                    const response = value.toString(10).concat(' ', from, ' is ', convertedValue, ' ', to);
+                    receivedCommand.channel.send(response);
+                } catch(err) {
+                    receivedCommand.channel.send("Something went wrong, check your units");
+                }
+            }
         } else {
             receivedCommand.channel.send("Invalid number of arguments. Use \\help cnv");
         }
--- feats/help.js
+++ feats/help.js
@@ -8,11 +8,9 @@ List of available commands:
         Use: '\\help command_name' for a detailed explanation of the command.
     \\toggle: Toggle features on or off.
     \\set: Change internal variables to customize behavior.
-    \\corgi or \\corgo: Fresh corgi content.
-    \\shibe or \\shiba: Fresh shibe content.
     \\remind: To help you remind of whatever you want.
-    \\wrong: When you're right.
-    \\pathetic: When you're right and disappointed.`);
+    \\cnv: For conversion between units.
+    \\Other commands: corgi/corgo, shibe/shiba, sabaton, wrong, pathetic, longestsong, aoty, demonbitch, brutal, manowar, pmsong`);
         } else {
             if (args[0] === 'help') {
                 receivedCommand.channel.send(`This command will help you out. However, asking for help on the help command is ridiculous.`);
@@ -20,10 +18,6 @@ List of available commands:
                 receivedCommand.channel.send(`Toggle a feature on or off. Use: '\\toggle list' to see what can be toggled.`);
             } else if (args[0] === 'set') {
                 receivedCommand.channel.send(`Set a variable value to something else. Use: '\\set list' to see what can be toggled.`);
-            } else if (args[0] === 'corgi' || args[0] === 'corgo') {
-                receivedCommand.channel.send(`Searches reddit for a good ol' corgo.`);
-            } else if (args[0] === 'shibe' || args[0] === 'shiba') {
-                receivedCommand.channel.send(`Searches reddit for a good ol' shiba.`);
             } else if (args[0] === 'remind') {
                 receivedCommand.channel.send(`\n
 Fabio will guestfully remind you of whatever you need, no matter how dirty ( ͡° ͜ʖ ͡°). Use:
@@ -32,10 +26,37 @@ Fabio will guestfully remind you of whatever you need, no matter how dirty ( ͡�
     * time_granularity can be minute(s), hour(s), day(s), month(s), year(s);
     * reminder_content is what you want to be reminded of;
     * NOTE: The 'in' and 'to' words need to be there, they don't need to be in or to respectively, they just need to be a word.`);
-            } else if (args[0] === 'wrong') {
-                receivedCommand.channel.send(`Share a nice argument-winning gif`);
-            } else if (args[0] === 'pathetic') {
-                receivedCommand.channel.send(`Share a nicer argument-winning gif`);
+            } else if (args[0] === 'cnv') {
+                receivedCommand.channel.send(`\n
+Fabio will convert values between units. Use:
+\\cnv value from_unit to_unit
+\\cnv list (for all types of unit categories)
+\\cnv list all (to list all possible units (be ready for a wall of text))
+\\cnv list category (to list all possible units within a category)
+    * Units can be:
+        * Currency (CAD, HKD, ISK, PHP, DKK, HUF, CZK, AUD, RON, SEK, IDR, INR, BRL, RUB, HRK, JPY, THB, CHF, SGD, PLN, BGN, TRY, CNY, NOK, NZD, ZAR, USD, MXN, ILS, GBP, KRW, MYR, EUR)
+        * Length (mm, cm, m, in, ft-us, ft, mi)
+        * Area (mm2, cm2, m2, ha, km2, in2, ft2, ac, mi2)
+        * Mass (mcg, mg, g, kg, oz, lb, mt, t)
+        * Volume (mm3, cm3, ml, l, kl, m3, km3, tsp, Tbs, in3, fl-oz, cup, pnt, qt, gal, ft3, yd3)
+        * Volume Flow Rate (mm3/s, cm3/s, ml/s, cl/s, dl/s, l/s, l/min, l/h, kl/s, kl/min, kl/h, m3/s, m3/min, m3/h, km3/s, tsp/s, Tbs/s, in3/s, in3/min, in3/h, fl-oz/s, fl-oz/min, fl-oz/h, cup/s, pnt/s, pnt/min, pnt/h, qt/s, gal/s, gal/min, gal/h, ft3/s, ft3/min, ft3/h, yd3/s, yd3/min, yd3/h)
+        * Temperature (C, F, K, R)
+        * Time (ns, mu, ms, s, min, h, d, week, month, year
+        * Frequency (Hz, mHz, kHz, MHz, GHz, THz, rpm, deg/s, rad/s)
+        * Speed (m/s, km/h, m/h, knot, ft/s)
+        * Pace (s/m, min/km, s/ft, min/km)
+        * Pressure (Pa, hPa, kPa, MPa, bar, torr, psi, ksi)
+        * Digital (b, Kb, Mb, Gb, Tb, B, KB, MB, GB, TB)
+        * Illuminance (lx, ft-cd)
+        * Parts-Per (ppm, ppb, ppt, ppq)
+        * Voltage (V, mV, kV)
+        * Current (A, mA, kA)
+        * Power (W, mW, kW, MW, GW)
+        * Apparent Power (VA, mVA, kVA, MVA, GVA)
+        * Reactive Power (VAR, mVAR, kVAR, MVAR, GVAR)
+        * Energy (Wh, mWh, kWh, MWh, GWh, J, kJ)
+        * Reactive Energy (VARh, mVARh, kVARh, MVARh, GVARh)
+        * Angle (deg, rad, grad, arcmin, arcsec)`);
             } else {
                 receivedCommand.channel.send(`Unknown argument (`.concat(args[0], `) for '\\help' command. Use only '\\help'.`));
             }
